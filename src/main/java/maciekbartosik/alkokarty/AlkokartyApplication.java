f108b5a99c4b3dacc92d35e2a189c0c8855b30d9
Maciek Bartosik <maciek@bartosik.wtf>
2021-05-16 13:50:03 +0200

    Setup

---
+++ src/main/java/maciekbartosik/alkokarty/AlkokartyApplication.java
@@ -0,0 +1,21 @@
+package maciekbartosik.alkokarty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class AlkokartyApplication {
+
+	@RequestMapping("/")
+	public String home() {
+		return "Hello Docker World test 5";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlkokartyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/maciekbartosik/alkokarty/AlkokartyApplicationTests.java
@@ -0,0 +1,13 @@
+package maciekbartosik.alkokarty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlkokartyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
